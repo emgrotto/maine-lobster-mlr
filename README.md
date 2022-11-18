017f1ebe8781e6dbc31568000c77049749715be9
Em.ma <emma@gc.com>
2022-11-18 18:56:56 -0500

    typo (#1)
    Co-authored-by: Emma Grotto <emmagrotto@Emmas-MBP.lan>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # maine-lobster-mlr
 Analysis of temperature data effects on lobster landings
+
+To run the project using Docker:
+
+```
+docker build -t lobster .
+docker run lobster
+docker run -it -v $(pwd)/etl/:/etl/ lobster bash
+```
---
+++ etl/mlr_model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+def create_design_matrix(cei, sea_max, sea_min, sea_avg):
+    number_obs = len(cei)
+    print(f"\nCreating the Desgin matrix for the {number_obs} observations")
+
+    ones = [1 for i in range(number_obs)]
+
+    sea_df = pd.DataFrame(list(zip(ones, cei, sea_max, sea_min, sea_avg)),
+                columns =['ones', 'cei', 'sea_max', 'sea_min', 'sea_avg'])
+
+    return sea_df
+
+def create_single_design_matrix(predictor):
+    number_obs = len(predictor)
+    print(f"\nCreating the Desgin matrix for the {number_obs} observations")
+
+    ones = [1 for i in range(number_obs)]
+
+    sea_df = pd.DataFrame(list(zip(ones, predictor)),
+                columns =['ones', 'predictor'])
+
+    return sea_df
+
+def predict_from_design(X, y): 
+
+    print('\nComputing parameter estimates')
+    Xt = np.transpose(X)
+    XtX = np.matmul(Xt,X)
+    Xty = np.matmul(Xt,y)
+    XtXinv = np.linalg.inv(XtX)
+    beta_hat = np.matmul(XtXinv,Xty)
+    print(beta_hat)
+    
+    print('\nPredicting Lobster Landings')
+    y_hat = np.matmul(X,beta_hat)
+    print(y_hat)
+    
+    print('\nComputing residuals')
+    r = y-y_hat
+    print(r)
+
+    print('\nVerifying Orthogonality')
+    orth = np.matmul(r, X)
+    print(orth)
+
+    return beta_hat, y_hat, r
